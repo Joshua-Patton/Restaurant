deebb8dbf588894999804274436c85cdfb6ae2dd
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2025-02-14 17:47:42 +0800

    webpack skeleton

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map", // soruce map- errors point to src code not dist code
+  devServer: { 
+    watchFiles: ["./src/template.html"], //webdeb only npx when template changes
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
